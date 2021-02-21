9cbca6b735993635a248013d923d267153a160ec
huliang <hu_liang56@163.com>
2021-02-21 14:49:02 +0800

    upgrade vite

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,19 +1,33 @@
 <template>
   <h1>{{ msg }}</h1>
-  <button @click="count++">count is: {{ count }}</button>
-  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+
+  <p>
+    <a href="https://vitejs.dev/guide/features.html" target="_blank"
+      >Vite Documentation</a
+    >
+    |
+    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
+  </p>
+
+  <button @click="state.count++">count is: {{ state.count }}</button>
+  <p>
+    Edit
+    <code>components/HelloWorld.vue</code> to test hot module replacement.
+  </p>
 </template>
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  },
-  data() {
-    return {
-      count: 0
-    }
-  }
-}
+<script setup>
+import { defineProps, reactive } from 'vue';
+
+defineProps({
+  msg: String,
+});
+
+const state = reactive({ count: 0 });
 </script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,17 @@
-module.exports = {
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import legacy from '@vitejs/plugin-legacy';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    legacy({
+      targets: ['defaults', 'not IE 11'],
+    }),
+  ],
   base: '/my-gallery/',
-  target: 'es2015',
-  assetsDir: 'assets',
-};
+  build: {
+    target: 'es2015',
+  },
+});
