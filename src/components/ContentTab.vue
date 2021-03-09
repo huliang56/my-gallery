5092452bfe5808bb46621f64dcf86a0a4ef76496
huliang <hu_liang56@163.com>
2021-03-09 19:08:47 +0800

    update contentTab

--- src/components/ContentTab.vue
+++ src/components/ContentTab.vue
@@ -1,20 +1,69 @@
 <template>
-  <van-tabs v-model:active="active" scrollspy sticky>
-    <van-tab title="标签 1">内容 1</van-tab>
-    <van-tab title="标签 2">内容 2</van-tab>
-    <van-tab title="标签 3">内容 3</van-tab>
-    <van-tab title="标签 4">内容 4</van-tab>
+  <van-tabs ref="tabs" v-model:active="active" sticky swipeable animated>
+    <van-tab v-for="group of CONFIG" :key="group.title" :title="group.title">
+      <div class="masonry">
+        <div
+          class="item"
+          v-for="(item, i) of group.images"
+          :key="item.url"
+          :style="{ paddingBottom: item.ratio }"
+        >
+          <van-image
+            :src="item.url"
+            lazy-load
+            @click="previewImage(group, i)"
+          />
+        </div>
+      </div>
+    </van-tab>
   </van-tabs>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { ref, inject } from 'vue';
+import { ImagePreview } from 'vant';
 export default {
   setup() {
     const active = ref(0);
+    const CONFIG = inject('CONFIG');
+
+    window.cacheImgs = (el) => {
+      const imgs = Array.from(el.querySelectorAll('img'));
+      return imgs.map((i) => ({
+        url: i.src,
+        ratio: ((i.naturalHeight / i.naturalWidth) * 100).toFixed(3) + '%',
+      }));
+    };
+
     return {
       active,
+      CONFIG,
     };
+  },
+  methods: {
+    previewImage(group, startPosition) {
+      const images = group.images.map((i) => i.url);
+      ImagePreview({ images, startPosition });
+    },
   },
 };
 </script>
+
+<style lang="scss">
+.masonry {
+  margin: 15px;
+  columns: 2;
+  column-gap: 10px;
+  .item {
+    height: 0;
+    margin-bottom: 10px;
+    background-color: #f7f8fa;
+  }
+  .van-image {
+    display: block;
+  }
+  img {
+    border-radius: 5px;
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,25 @@
 import { createApp } from 'vue';
-import { Swipe, SwipeItem, ActionSheet, Tab, Tabs } from 'vant';
+import {
+  Image as VanImage,
+  Loading,
+  Swipe,
+  SwipeItem,
+  ActionSheet,
+  Tab,
+  Tabs,
+  Lazyload,
+} from 'vant';
 import 'vant/lib/index.css';
 import App from './App.vue';
 import './index.css';
 
 createApp(App)
+  .use(VanImage)
+  .use(Loading)
   .use(Swipe)
   .use(SwipeItem)
   .use(ActionSheet)
   .use(Tab)
   .use(Tabs)
+  .use(Lazyload)
   .mount('#app');
